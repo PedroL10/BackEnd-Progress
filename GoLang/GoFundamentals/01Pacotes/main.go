4428a5c6672426ec20319da8baf2b695d8bce05b
PedroLGon <pepluisgon01@gmail.com>
2024-10-24 18:19:28 -0300

    External packages

--- GoLang/GoFundamentals/01Pacotes/main.go
+++ GoLang/GoFundamentals/01Pacotes/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("pep@gmail.com")
+	erro1 := checkmail.ValidateFormat("pep")
+	fmt.Print(erro)
+	fmt.Print(erro1)
 }
