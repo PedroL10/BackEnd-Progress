469919706e38e769403e7621700a7b721710cdc1
PedroLGon <pepluisgon01@gmail.com>
2024-10-25 16:20:30 -0300

    functions

---
+++ GoLang/GoFundamentals/04Funcoes/funcoes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func somar(n1 int, n2 int) int {
+	return n1 + n2
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
+
+func main() {
+	soma := somar(15, 20)
+	fmt.Println(soma)
+
+	var f = func() {
+		fmt.Println("Funcao F")
+	}
+
+	f()
+
+	resultadoSoma, resultadoSubtracao := calculosMatematicos(10, 15)
+	fmt.Println(resultadoSoma, resultadoSubtracao)
+
+}
